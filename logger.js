41976f263b175783798694b50132141d9d590841
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-08 16:42:03 -0600

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,13 @@
+// logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(info => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
+  ),
+  transports: [new winston.transports.Console()]
+});
+
+module.exports = logger;
