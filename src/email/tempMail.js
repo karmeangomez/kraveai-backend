554cc73c3e767086aa2b50058e1e394a3dee3e3c
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-21 14:50:11 -0600

    Update tempMail.js

--- src/email/tempMail.js
+++ src/email/tempMail.js
@@ -1,14 +1,6 @@
 export default class TempMail {
   async getEmailAddress() {
-    try {
-      // API de Temp-Mail.org
-      const response = await axios.get('https://api.temp-mail.org/request/domains/format/json');
-      const domains = response.data;
-      const randomDomain = domains[Math.floor(Math.random() * domains.length)];
-      const prefix = Math.random().toString(36).substring(2, 10);
-      return `${prefix}@${randomDomain}`;
-    } catch (error) {
-      throw new Error(`TempMail failed: ${error.message}`);
-    }
+    const prefix = Math.random().toString(36).substring(2, 10);
+    return `${prefix}@tempmail.net`;
   }
 }
