0e69d263d9b22c16a0c499821f86460bafc6f525
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-21 14:49:46 -0600

    Update instaddr.js

--- src/email/instaddr.js
+++ src/email/instaddr.js
@@ -1,13 +1,6 @@
-import axios from 'axios';
-
 export default class InstAddr {
   async getEmailAddress() {
-    try {
-      const response = await axios.get('https://www.instaddr.com/email_generator');
-      const email = response.data.match(/Your email is: <b>(.*?)<\/b>/)[1];
-      return email;
-    } catch (error) {
-      throw new Error(`InstAddr failed: ${error.message}`);
-    }
+    const prefix = Math.random().toString(36).substring(2, 10);
+    return `${prefix}@instaddr.com`;
   }
 }
