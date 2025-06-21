c1c35c0e98bbfa1dd9c8ab4762858c51a093ca5a
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-21 14:50:35 -0600

    Update oneSecMail.js

--- src/email/oneSecMail.js
+++ src/email/oneSecMail.js
@@ -1,11 +1,6 @@
 export default class OneSecMail {
   async getEmailAddress() {
-    try {
-      // Generar email aleatorio con OneSecMail
-      const randomString = Math.random().toString(36).substring(2, 10);
-      return `${randomString}@1secmail.com`;
-    } catch (error) {
-      throw new Error(`OneSecMail failed: ${error.message}`);
-    }
+    const prefix = Math.random().toString(36).substring(2, 10);
+    return `${prefix}@1secmail.com`;
   }
 }
