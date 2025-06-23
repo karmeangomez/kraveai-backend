5a23837c3768913ed4493611302645c8362ce61b
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-06-23 01:35:15 -0600

    Update mailtm.js

--- src/email/mailtm.js
+++ src/email/mailtm.js
@@ -1,38 +1,58 @@
 import axios from 'axios';
 
 export default {
-  name: 'GuerrillaMail',
-  sessionToken: null,
+  name: 'MailTM',
+  account: null,
   
-  async initSession() {
-    const response = await axios.get('https://api.guerrillamail.com/ajax.php?f=get_email_address');
-    this.sessionToken = response.data.sid_token;
-    return response.data.email_addr;
+  async createAccount() {
+    // 1. Obtener dominio
+    const domainRes = await axios.get('https://api.mail.tm/domains');
+    const domain = domainRes.data['hydra:member'][0].domain;
+    
+    // 2. Crear cuenta
+    const address = `${Math.random().toString(36).substring(2, 10)}@${domain}`;
+    const password = Math.random().toString(36);
+    
+    const accountRes = await axios.post('https://api.mail.tm/accounts', {
+      address,
+      password
+    });
+    
+    this.account = { address, password, id: accountRes.data.id };
+    return address;
   },
 
   async getEmailAddress() {
-    if (!this.sessionToken) return this.initSession();
-    
-    const response = await axios.get(`https://api.guerrillamail.com/ajax.php?f=get_email_address&sid_token=${this.sessionToken}`);
-    return response.data.email_addr;
+    if (!this.account) return this.createAccount();
+    return this.account.address;
   },
 
   async getVerificationCode(email) {
-    if (!this.sessionToken) return null;
+    if (!this.account) return null;
     
-    const response = await axios.get(`https://api.guerrillamail.com/ajax.php?f=get_email_list&offset=0&sid_token=${this.sessionToken}`);
-    const emails = response.data.list;
+    // 1. Obtener token
+    const tokenRes = await axios.post('https://api.mail.tm/token', {
+      address: this.account.address,
+      password: this.account.password
+    });
     
-    const instagramEmail = emails.find(e => 
-      e.mail_subject.includes('Instagram') && 
-      e.mail_recipient === email
+    // 2. Buscar email de Instagram
+    const messagesRes = await axios.get('https://api.mail.tm/messages', {
+      headers: { Authorization: `Bearer ${tokenRes.data.token}` }
+    });
+    
+    const instagramMsg = messagesRes.data['hydra:member'].find(m => 
+      m.subject.includes('Instagram') && 
+      m.to[0].address === email
     );
     
-    if (instagramEmail) {
-      const emailResponse = await axios.get(`https://api.guerrillamail.com/ajax.php?f=fetch_email&email_id=${instagramEmail.mail_id}&sid_token=${this.sessionToken}`);
-      const body = emailResponse.data.mail_body;
-      const codeMatch = body.match(/>(\d{6})</);
-      return codeMatch ? codeMatch[1] : null;
+    if (instagramMsg) {
+      const messageRes = await axios.get(`https://api.mail.tm/messages/${instagramMsg.id}`, {
+        headers: { Authorization: `Bearer ${tokenRes.data.token}` }
+      });
+      
+      const codeMatch = messageRes.data.text.match(/\d{6}/);
+      return codeMatch ? codeMatch[0] : null;
     }
     return null;
   }
