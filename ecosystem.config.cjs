f9c3a063e55713e7c90c0ffc74a9fed076a94026
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-07-19 13:56:05 -0600

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,16 +3,17 @@ module.exports = {
     {
       name: "backend",
       cwd: "/home/karmean/kraveai-backend",
-      script: "bash",
-      args: "-c 'source /home/karmean/kraveai-backend/venv/bin/activate && uvicorn src.main:app --host 0.0.0.0 --port 8000'",
-      interpreter: "/bin/bash",
+      script: "/home/karmean/kraveai-backend/venv/bin/python",
+      args: "src/main.py",
+      interpreter: "/home/karmean/kraveai-backend/venv/bin/python",
       watch: false,
-      log_date_format: "YYYY-MM-DD HH:mm Z",
-      error_file: "/home/karmean/kraveai-backend/logs/error.log",
-      out_file: "/home/karmean/kraveai-backend/logs/out.log",
-      combine_logs: true,
-      time: true
-    }
-  ]
+      env: {
+        PYTHONUNBUFFERED: "1",
+      },
+      error_file: "logs/error.log",
+      out_file: "logs/out.log",
+      merge_logs: true,
+      time: true,
+    },
+  ],
 };
-
