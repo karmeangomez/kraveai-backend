6bad0f77a4c1b4fd3598f416487d18513d2445fc
Karmean Gómez <karmean@example.com>
2025-07-21 14:22:35 -0600

    Auto-push desde Raspberry - index.lock

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source /home/karmean/kraveai-backend/venv/bin/activate
-cd /home/karmean/kraveai-backend
-uvicorn src.main:app --host 0.0.0.0 --port 8000
+source ~/kraveai-backend/venv/bin/activate
+exec python ~/kraveai-backend/src/main.py
