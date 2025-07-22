db024202d00a3d64e2e12b2823e9783ef25f0fa3
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-07-22 12:44:19 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/karmean/kraveai-backend/
 source venv/bin/activate
-uvicorn src.main:app --host 127.0.0.1 --port 8000 --reload
+exec uvicorn src.main:app --host 127.0.0.1 --port 8000
