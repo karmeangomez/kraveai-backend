03d64ddf73ebc6e1d10193ab503330efdbe931b6
karmeangomez <66385426+karmeangomez@users.noreply.github.com>
2025-07-28 12:02:07 -0600

    Update iniciar_sesion_kraveaibot.py

--- iniciar_sesion_kraveaibot.py
+++ iniciar_sesion_kraveaibot.py
@@ -1,23 +1,17 @@
 import os
 from dotenv import load_dotenv
-from src.login_utils import login_instagram, guardar_sesion, verificar_sesion
+from src.login_utils import login_instagram, guardar_sesion
 
 load_dotenv()
-
-# Leer credenciales desde .env
 username = os.getenv("INSTA_USER")
 password = os.getenv("INSTA_PASS")
-
-if not username or not password:
-    print("❌ No se encontraron credenciales en el archivo .env")
-    exit(1)
 
 print(f"🔐 Iniciando sesión para: {username}...")
 
 cl = login_instagram(username, password)
 
-if cl and verificar_sesion(cl, username):
+if cl:
+    print(f"✅ Sesión iniciada correctamente para {username}")
     guardar_sesion(cl, username)
-    print(f"✅ Sesión iniciada y guardada correctamente para {username}")
 else:
     print(f"❌ No se pudo iniciar sesión para {username}")
